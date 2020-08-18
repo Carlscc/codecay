afd69e0577a7a27682866660737a610ec40d4eec
Caroline Chan <carolinechanccc@gmail.com>
2020-08-18 16:51:39 +0100

    add models and migrations

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime
+from hosts.models import Host
 
-# Create your models here.
+class Listing(models.Model):
+    host = models.ForeignKey(Host, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    outdoor = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    def __str__(self):
+        return self.title
+
+
+
