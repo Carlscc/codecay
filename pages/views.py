75de6c93e8043d98a5811767db225e8760cafc0d
Caroline Chan <carolinechanccc@gmail.com>
2020-08-21 11:12:48 +0100

    pull hosts data to about page, fix formatting

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from listings.models import Host
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
@@ -12,5 +13,11 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    hosts = Host.objects.all()
+
+    context = {
+        'hosts': hosts
+    }
+
+    return render(request, 'pages/about.html', context)
 
