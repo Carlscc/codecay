99df1da72327feaddb0995ead0a18a08bbecfdf9
Caroline Chan <carolinechanccc@gmail.com>
2020-08-21 15:53:48 +0100

    add search form choices

---
+++ listings/choices.py
@@ -0,0 +1,33 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4
+}
+
+price_choices = {
+    '50': '£50',
+    '60': '£60',
+    '70': '£70',
+    '80': '£80',
+    '90': '£90',
+    '100': '£100',
+    '110': '£110',
+    '120': '£120',
+    '130': '£130',
+    '140': '£140',
+    '150': '£150',
+}
+
+county_choices = {
+    'London': 'London',
+    'Lancashire': 'Lancashire',
+    'East Sussex': 'East Sussex',
+    'West Sussex': 'West Sussex',
+    'Berkshire': 'Berkshire',
+    'Hampshire': 'Hampshire',
+    'Oxfordshire': 'Oxfordshire',
+    'Surrey': 'Surrey',
+
+}
+
--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, county_choices
 
 from .models import Listing
 
@@ -11,7 +12,7 @@ def index(request):
     paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': paged_listings
+        'listings': paged_listings,
     }
 
     return render(request, 'listings/listings.html', context)
@@ -25,4 +26,9 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'county_choices': county_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html', context)
